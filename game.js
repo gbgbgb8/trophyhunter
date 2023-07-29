80997d902fbdde69dd9855f9dcc86643050754fd
Nota Name <email@thisisnotanemail.com>
2023-07-29 16:08:47 -0700

    revert

--- game.js
+++ game.js
@@ -120,10 +120,3 @@ for(let i = 0; i < gridSize; i++) {
 
     grid.appendChild(cell);
 }
-
-
-window.onload = function() {
-    setTimeout(function() {
-        window.scrollTo(0, 1);
-    }, 0);
-};
